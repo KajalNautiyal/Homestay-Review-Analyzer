27173f310f6aa364c027a63ddd2ed1a165299a8d
Kajal Nautiyal <kajalnautiyal205@gmail.com>
2026-06-23 18:06:14 +0530

    Add reusable Loader component

---
+++ app/components/ui/Loader.jsx
@@ -0,0 +1,11 @@
+/**
+ * Loader Component
+ */
+
+export default function Loader() {
+  return (
+    <div className="flex justify-center">
+      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-500"></div>
+    </div>
+  );
+}
